61e469d1ba68499fe983341a2792097f39972c18
Thiago Durante Pires <thdurante@gmail.com>
2017-04-10 19:39:10 -0300

    Comparando dois funcionários com os mesmos atributos Exercício 4

--- src/main/java/br/ufg/inf/TestaFuncionario.java
+++ src/main/java/br/ufg/inf/TestaFuncionario.java
@@ -6,11 +6,17 @@ public class TestaFuncionario {
 
     public static void main(String[] args) {
         Funcionario f1 = new Funcionario();
-
-        f1.setNome("Hugo");
+        f1.setNome("Danilo");
         f1.setSalario(100);
-        f1.recebeAumento(50);
 
-        f1.mostra();
+        Funcionario f2 = new Funcionario();
+        f2.setNome("Danilo");
+        f2.setSalario(100);
+
+        if (f1 == f2) {
+            System.out.println("iguais");
+        } else {
+            System.out.println("diferentes");
+        }
     }
 }
